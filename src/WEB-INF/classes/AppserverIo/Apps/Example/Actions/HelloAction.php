d00c84720b7469cec9a2f214e6b7df20836ab689
Tim Wagner <t.wagner@techdivision.com>
2016-01-22 23:39:05 +0100

    Refactor for last version of securit subsystem

---
+++ src/WEB-INF/classes/AppserverIo/Apps/Example/Actions/HelloAction.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * AppserverIo\Apps\Example\Actions\HelloAction
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-apps/example
+ * @link      http://www.appserver.io
+ */
+
+namespace AppserverIo\Apps\Example\Actions;
+
+use AppserverIo\Routlt\DispatchAction;
+use AppserverIo\Routlt\ActionInterface;
+use AppserverIo\Apps\Example\Entities\Impl\Sample;
+use AppserverIo\Apps\Example\Utils\RequestKeys;
+use AppserverIo\Psr\Servlet\Http\HttpServletRequestInterface;
+use AppserverIo\Psr\Servlet\Http\HttpServletResponseInterface;
+
+/**
+ * Example action implementation that loads data over a persistence container proxy
+ * and renders a list, based on the returned values.
+ *
+ * Additional it provides functionality to edit, delete und persist the data after
+ * changing it.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-apps/example
+ * @link      http://www.appserver.io
+ *
+ * @Path(name="/hello")
+ *
+ * @Results({
+ *     @Result(name="input", result="input", type="AppserverIo\Routlt\Results\RawResult"),
+ *     @Result(name="failure", result="failure", type="AppserverIo\Routlt\Results\RawResult")
+ * })
+ *
+ */
+class HelloAction extends DispatchAction
+{
+
+    /**
+     * Default action to invoke if no action parameter has been found in the request.
+     *
+     * Loads all sample data and attaches it to the servlet context ready to be rendered
+     * by the template.
+     *
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletRequestInterface  $servletRequest  The request instance
+     * @param \AppserverIo\Psr\Servlet\Http\HttpServletResponseInterface $servletResponse The response instance
+     *
+     * @return string|null The action result
+     *
+     * @Action(name="/index")
+     */
+    public function indexAction(HttpServletRequestInterface $servletRequest, HttpServletResponseInterface $servletResponse)
+    {
+        $servletRequest->setAttribute(ActionInterface::INPUT, 'Hello World from Action!');
+    }
+}
