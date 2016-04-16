a01ea78d267cbb31162f4407370561005148e48c
wagnert <wagner_tim78@hotmail.com>
2016-04-16 22:34:37 +0200

    Document robo file

--- RoboFile.php
+++ RoboFile.php
@@ -1,31 +1,63 @@
 <?php
 
 /**
- * This is project's console commands configuration for Robo task runner.
+ * phpinfo.php
  *
- * @see http://robo.li/
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-apps/example
+ * @link      http://www.appserver.io
  */
 
 use Lurker\Event\FilesystemEvent;
 use AppserverIo\Properties\Properties;
 use AppserverIo\Properties\PropertiesUtil;
 
+/**
+ * Defines the available build tasks.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2016 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io-apps/example
+ * @link      http://www.appserver.io
+ */
 class RoboFile extends \Robo\Tasks
 {
 
+    /**
+     * The process instance for the application server's runner.
+     *
+     * @var \Robo\Result
+     */
     protected $serverProcess = null;
 
-    protected $result = null;
+    /**
+     * The build properties.
+     *
+     * @var \AppserverIo\Appserver\PropertiesInterface
+     */
+    protected $properties = null;
 
-    protected $containers = array(
-        'appdata'
-    );
-
+    /**
+     * Initialize's the tasks.
+     */
     public function __construct()
     {
 
+        // initialize the build properties
         $this->properties = Properties::create();
 
+        // set the default properties
         $this->properties->setProperty('src.dir', 'src');
         $this->properties->setProperty('vendor.dir', '${src.dir}/vendor');
         $this->properties->setProperty('base.dir', '/opt/appserver');
@@ -34,12 +66,25 @@ class RoboFile extends \Robo\Tasks
         $this->properties->setProperty('configuration.file', __DIR__ . '/etc/appserver/appserver.xml');
         $this->properties->setProperty('bootstrap.file', '${base.dir}/etc/appserver/conf.d/bootstrap-runner.xml');
 
-        $this->properties->merge(Properties::create()->load(__DIR__ . '/build.properties'));
-        $this->properties->merge(Properties::create()->load(__DIR__ . '/build.default.properties'));
+        // load properties from build.properties file
+        if (file_exists($buildProperties = __DIR__ . '/build.properties')) {
+            $this->properties->mergeProperties(Properties::create()->load($buildProperties));
+        }
 
+        // load the default build properties
+        if (file_exists($buildDefaultProperties = __DIR__ . '/build.default.properties')) {
+            $this->properties->mergeProperties(Properties::create()->load($buildDefaultProperties));
+        }
+
+        // replace the variables in the properties
         PropertiesUtil::singleton()->replaceProperties($this->properties);
     }
 
+    /**
+     * Run's the composer install command.
+     *
+     * @return void
+     */
     public function composerInstall()
     {
         // optimize autoloader with custom path
@@ -49,6 +94,11 @@ class RoboFile extends \Robo\Tasks
              ->run();
     }
 
+    /**
+     * Run's the composer update command.
+     *
+     * @return void
+     */
     public function composerUpdate()
     {
         // optimize autoloader with custom path
@@ -58,18 +108,53 @@ class RoboFile extends \Robo\Tasks
              ->run();
     }
 
+    /**
+     * Clean up the environment for a new build.
+     *
+     * @return void
+     */
+    public function clean()
+    {
+        $this->taskDeleteDir($this->properties->getProperty('target.dir'))->run();
+    }
+
+    /**
+     * Prepare's the environment for a new build.
+     *
+     * @return void
+     */
+    public function prepare()
+    {
+        $this->taskFileSystemStack()->mkdir($this->properties->getProperty('target.dir'))->run();
+    }
+
+    /**
+     * Calculate's the next version version number based on the actual
+     * version defined in the the build.default.properties file.
+     *
+     * @return void
+     */
     public function semverCompare()
     {
 
-        $this->taskDeleteDir($this->properties->getProperty('target.dir'))->run();
-        $this->taskFileSystemStack()->mkdir($this->properties->getProperty('target.dir'));
+        // prepare the environment
+        $this->clean();
+        $this->prepare();
 
+        // reset the .semver file
+        $this->taskGitStack()
+            ->exec('checkout .semver')
+            ->stopOnFail()
+            ->run();
+
+        // clone the tag of the last version defined in the build properties
         $this->taskGitStack()
              ->cloneRepo('git@github.com:wagnert/example.git', sprintf('%s/example', $this->properties->getProperty('target.dir')))
              ->stopOnFail()
-             ->pull('origin', '2.1.11')
+             ->pull('origin', $this->properties->getProperty('appserver.webapp.version'))
              ->run();
 
+        // analyze the differences between the two versions and write them to an JSON file
         $this->taskExec(sprintf('%s/bin/php-semver-checker', $this->properties->getProperty('vendor.dir')))
              ->arg('compare')
              ->arg('.')
@@ -77,13 +162,25 @@ class RoboFile extends \Robo\Tasks
              ->arg(sprintf('--to-json=%s/semver.json', $this->properties->getProperty('target.dir')))
              ->run();
 
+        // read the incrementation level from the written JSON file
         $semver = json_decode(file_get_contents(sprintf('%s/semver.json', $this->properties->getProperty('target.dir'))));
 
+        // query whether or not we've to raise the version
+        if (($level = strtolower($semver->level)) === 'none') {
+            return;
+        }
+
+        // increment the .semver file with the new version
         $this->taskSemVer('.semver')
-             ->increment(strtolower($semver->level))
+             ->increment($level)
              ->run();
     }
 
+    /**
+     * Run's the PHPUnit tests.
+     *
+     * @return void
+     */
     public function runTests()
     {
         $this->taskPHPUnit(sprintf('%s/bin/phpunit', $this->properties->getProperty('vendor.dir')))
@@ -92,6 +189,11 @@ class RoboFile extends \Robo\Tasks
              ->run();
     }
 
+    /**
+     * Run's the PHPSpec tests.
+     *
+     * @return void
+     */
     public function runSpec()
     {
         $this->taskPhpspec(sprintf('%s/bin/phpspec', $this->properties->getProperty('vendor.dir')))
@@ -101,14 +203,22 @@ class RoboFile extends \Robo\Tasks
              ->run();
     }
 
+    /**
+     * Deploy's the application to the application server's webapps dir.
+     *
+     * @return void
+     */
     public function deploy()
     {
         $this->taskCopyDir([$this->properties->getProperty('src.dir') => $this->properties->getProperty('deploy.dir')])->run();
     }
 
+    /**
+     * Sync's the application sources from the src directory and
+     * the application server's webapps dir, using rsync.
+     */
     public function rsync()
     {
-
         $this->taskRsync()
              ->fromPath('src/')
              ->toPath($this->properties->getProperty('deploy.dir'))
@@ -125,18 +235,26 @@ class RoboFile extends \Robo\Tasks
              ->run();
     }
 
+    /**
+     * Stop's the application server's runner.
+     *
+     * @return void
+     */
     public function stop()
     {
-
         if ($this->serverProcess) {
             $task = $this->serverProcess->getTask();
             $task->__destruct();
         }
     }
 
+    /**
+     * Start's the application server's runner.
+     *
+     * @return void
+     */
     public function start()
     {
-
         $this->serverProcess = $this->taskExec('php /opt/appserver/server.php')
                                     ->arg(sprintf('-b=%s', $this->properties->getProperty('bootstrap.file')))
                                     ->arg(sprintf('-c=%s', $this->properties->getProperty('configuration.file')))
@@ -145,104 +263,23 @@ class RoboFile extends \Robo\Tasks
                                     ->run();
     }
 
+    /**
+     * Watch the src folder for changes and restart's
+     * the application server's runner if necessary.
+     *
+     * @return void
+     */
     public function watch()
     {
 
+        // initially start the application server runner
         $this->start();
 
+        // watch the src directory for any changes
         $this->taskWatch()->monitor($this->properties->getProperty('src.dir'), function(FilesystemEvent $event) {
-
+            // restart the application server's runner
             $this->stop();
             $this->start();
-
         })->run();
-    }
-
-    /**
-     * Deploy's the source files to the docker containers.
-     *
-     * @return void
-     */
-    public function dockerDeploy()
-    {
-        foreach ($this->containers as $container) {
-            $this->taskExec(sprintf("docker cp src $container:%s/", $this->properties->getProperty('deploy.dir')))->run();
-        }
-    }
-
-    public function dockerStop()
-    {
-
-        $this->taskDockerStop('example')
-             ->run();
-
-        $this->taskDockerRemove('example')
-             ->run();
-    }
-
-    public function dockerRun()
-    {
-
-         $this->taskDockerBuild()
-              ->tag('example')
-              ->run();
-
-         $this->taskDockerRun('example')
-              ->interactive()
-              ->name('example')
-              ->option('--rm')
-              ->run();
-    }
-
-    public function dockerWatch()
-    {
-
-        $this->dockerRun();
-
-        $this->taskWatch()
-             ->monitor('src', function () {
-                 $this->dockerStop();
-                 $this->dockerRun();
-             }
-        )->run();
-    }
-
-    /**
-     * Synchronizes the containers when a file changes.
-     *
-     * @return void
-     */
-    public function dockerSync()
-    {
-
-        $this->taskWatch()
-            ->monitor('src', function(FilesystemEvent $event) {
-
-                foreach ($this->containers as $container) {
-
-                    $relativePath = str_replace(__DIR__ . '/src/', '', $event->getResource());
-
-                    $this->taskExec(
-                        sprintf(
-                            'docker exec -t %s mkdir -p %s/%s',
-                            $container,
-                            $this->properties->getProperty('deploy.dir'),
-                            dirname($relativePath)
-                        )
-                    )->run();
-
-                    $this->taskExec(
-                        sprintf(
-                            'docker cp %s %s:%s/%s',
-                            $event->getResource(),
-                            $container,
-                            $this->properties->getProperty('deploy.dir'),
-                            $relativePath
-                        )
-                    )->run();
-                }
-            }
-
-        )->run();
     }
 }
